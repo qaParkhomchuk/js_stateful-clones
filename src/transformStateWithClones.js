86c33b6c86a296073edd5e96f954d34d6b4f2702
Nataliia Parkhomchuk <n.polishchuk83@gmail.com>
2025-02-14 17:29:04 +0200

    my task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,23 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const history = [];
+
+  actions.forEach(action => {
+    if (action.type === 'clear') {
+      currentState = {};
+    } else if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = Object.fromEntries(
+        Object.entries(currentState).filter(([key]) => !action.keysToRemove.includes(key))
+      );
+    }
+    history.push({ ...currentState });
+    });
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
